c552c7f46901f76ce16bbaba2fd55cf33f58f054
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2026-02-22 14:16:04 +0530

    updated

--- backend/src/validations/userValidation.js
+++ backend/src/validations/userValidation.js
@@ -1,10 +1,20 @@
 import Joi from 'joi';
 
 export const registerSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
+    email: Joi.string().email().required().messages({
+        'string.email': 'Please provide a valid email',
+        'any.required': 'Email is required',
+    }),
+    password: Joi.string().min(6).required().messages({
+        'string.min': 'Password must be at least 6 characters',
+        'any.required': 'Password is required',
+    }),
+    firstName: Joi.string().required().messages({
+        'any.required': 'First name is required',
+    }),
+    lastName: Joi.string().required().messages({
+        'any.required': 'Last name is required',
+    }),
     household: Joi.object({
         address: Joi.string().optional(),
         size: Joi.number().integer().min(1).optional(),
