894f3d8f5bb5fdb0d9785655386d2c11f0d70b1f
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2026-02-23 14:58:31 +0530

    update

--- powerguard-frontend/src/main.jsx
+++ powerguard-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import App from './App'
 import { AuthProvider } from './context/AuthContext'
 import { ToastProvider } from './context/ToastContext'
 import './index.css'
@@ -15,5 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                 </ToastProvider>
             </AuthProvider>
         </BrowserRouter>
-    </React.StrictMode>,
+    </React.StrictMode>
 )
